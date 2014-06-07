d260d0d8e986f23f0d474f0fa147b03d1f85e5cf
wenzhen huang <coderhwz@gmail.com>
2014-06-07 15:33:21 +0800

    添加文章时不开启自动保存

--- pure/static/js/admin.js
+++ pure/static/js/admin.js
@@ -1,19 +1,22 @@
 (function(){
     $(document).ready(function(){
         var postForm = $('.post-form');
-        if (postForm.length > 0) {
+        if (postForm.length > 0 ) {
 
             var editor = CodeMirror.fromTextArea(document.getElementById('editor'),{
                 viewportMargin:Infinity,
             });
-            setInterval(function(){
-                var postData = {};
-                $('input',postForm).each(function(){
-                    postData[$(this).attr('name')] = $(this).val();
-                }); 
-                postData['markdown'] = editor.getValue();
-                $.post(postForm.attr('action'),postData,function(){});
-            },10000);
+            var id = $('input[name=id]');
+            if (id.val() > 0) {
+                setInterval(function(){
+                    var postData = {};
+                    $('input',postForm).each(function(){
+                        postData[$(this).attr('name')] = $(this).val();
+                    }); 
+                    postData['markdown'] = editor.getValue();
+                    $.post(postForm.attr('action'),postData,function(){});
+                },10000);
+            }
         }
     });
 })(jQuery)
