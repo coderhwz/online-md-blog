19960d672d7b0a1a594813bdb4583bccf67c39ab
wenzhen huang <coderhwz@gmail.com>
2014-06-17 17:23:30 +0800

    编辑页面添加Ctrl-S保存

--- pure/static/js/admin.js
+++ pure/static/js/admin.js
@@ -1,22 +1,39 @@
 (function(){
     $(document).ready(function(){
         var postForm = $('.post-form');
+
         if (postForm.length > 0 ) {
+            var saveBtn = $('.save');
+            var save_post = function() {
+                var postData = {};
+                $('input',postForm).each(function(){
+                    postData[$(this).attr('name')] = $(this).val();
+                }); 
+                postData['markdown'] = editor.getValue();
+                saveBtn.val('保存...').css('background-color','red');
+
+                $.post(postForm.attr('action'),postData,function(){
+                    setTimeout(function(){
+                        saveBtn.val('保存').css('background-color','#3276b1');
+                    },500);
+                });
+            };
 
             var editor = CodeMirror.fromTextArea(document.getElementById('editor'),{
                 viewportMargin:Infinity,
             });
             var id = $('input[name=id]');
             if (id.val() > 0) {
-                setInterval(function(){
-                    var postData = {};
-                    $('input',postForm).each(function(){
-                        postData[$(this).attr('name')] = $(this).val();
-                    }); 
-                    postData['markdown'] = editor.getValue();
-                    $.post(postForm.attr('action'),postData,function(){});
-                },10000);
+                setInterval(save_post,10000);
             }
+            $(window).bind('keydown', function(event) {
+                if (event.ctrlKey || event.metaKey) {
+                    if (String.fromCharCode(event.which).toLowerCase() == 's') {
+                        event.preventDefault();
+                        save_post();
+                    }
+                }
+            });
         }
     });
 })(jQuery)
